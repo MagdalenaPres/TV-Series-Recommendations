ca1192b561aa6cd35f5c1e07b0da8a1dc4123bef
Magdalena Preś <magdalenapres000@gmail.com>
2022-01-06 22:21:27 +0100

    update home and add tvseries

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,27 @@
 import React, {Component} from 'react';
+import Select from 'react-select'
 
 var Background = "https://hdwallpaperim.com/wp-content/uploads/2017/08/23/472571-film_reel-movies-748x403.jpg";
 var btn = document.getElementById("create_btn"); 
-var div_list = document.getElementById("w_list");
+const options = [
+    { value: '10', label: '10' },
+    { value: '9', label: '9' },
+    { value: '8', label: '8' },
+    { value: '7', label: '7' },
+    { value: '6', label: '6' },
+    { value: '5', label: '5' },
+    { value: '4', label: '4' },
+    { value: '3', label: '3' },
+    { value: '2', label: '2' },
+    { value: '1', label: '1' }
+  ]
 
 class Home extends Component {
+    
     constructor(props) {
         super(props);
+        this.div_list = React.createRef();
+
         this.create_btn = this.create_btn.bind(this);
         this.create_list = this.create_list.bind(this);
         this.add_to_end = this.add_to_end.bind(this);
@@ -14,31 +29,36 @@ class Home extends Component {
         this.remove_from_end = this.remove_from_end.bind(this);
         this.submit_list = this.submit_list.bind(this);
         this.reset_list = this.reset_list.bind(this);
+        this.showAlert = this.showAlert.bind(this);
+        this.check_avg = this.check_avg.bind(this);
       }
 
     create_btn()
     {
         btn = document.getElementById("create_btn"); 
         btn.remove();
+        
         this.create_list();
     }
     create_list(){
-        div_list.classList.add("show");
+        const node = this.div_list.current;
+
+        node.classList.add("show");
 
         var button_append = document.createElement("BUTTON");
-        button_append.innerHTML = "Dodaj na koniec";
+        button_append.innerHTML = "Add at the end";
 
         var button_before = document.createElement("BUTTON");
-        button_before.innerHTML = "Dodaj na początek";
+        button_before.innerHTML = "Add at the beginning";
 
         var button_remove = document.createElement("BUTTON");
-        button_remove.innerHTML = "Usuń element z końca";
+        button_remove.innerHTML = "Delete from the end";
 
         var button_submit = document.createElement("BUTTON");
-        button_submit.innerHTML = "Wyślij";
+        button_submit.innerHTML = "Send";
 
         var button_reset = document.createElement("BUTTON");
-        button_reset.innerHTML = "Resetuj";
+        button_reset.innerHTML = "Reset";
 
         button_append.classList.add("wListBtn");
         button_before.classList.add("wListBtn");
@@ -47,17 +67,17 @@ class Home extends Component {
         button_submit.classList.add("wListBtn");
         button_reset.classList.add("wListBtn");
 
-        div_list.appendChild(button_append);
-        div_list.appendChild(button_before);
-        div_list.appendChild(button_remove);
-        div_list.appendChild(button_submit);
-        div_list.appendChild(button_reset);
+        node.appendChild(button_append);
+        node.appendChild(button_before);
+        node.appendChild(button_remove);
+        node.appendChild(button_submit);
+        node.appendChild(button_reset);
 
-        button_append.addEventListener("click", function() { this.add_to_end(); });
-        button_before.addEventListener("click", function() { this.add_to_beginning(); });
-        button_remove.addEventListener("click", function() { this.remove_from_end(); });
-        button_submit.addEventListener("click", function() { this.submit_list(); });
-        button_reset.addEventListener("click", function() { this.reset_list(); });
+        button_append.addEventListener("click", event => { this.add_to_end(); });
+        button_before.addEventListener("click", event => { this.add_to_beginning(); });
+        button_remove.addEventListener("click", event => { this.remove_from_end(); });
+        button_submit.addEventListener("click", event => { this.submit_list(); });
+        button_reset.addEventListener("click", event => { this.reset_list(); });
     }
 add_to_end()
 {
@@ -101,7 +121,7 @@ reset_list()
 
 pressed_enter = (event) => {
     var x = event.keyCode;
-    if (x == 13) {  
+    if (x === 13) {  
       this.add_to_end();
     }
   }
@@ -111,17 +131,94 @@ pressed_enter = (event) => {
                 <section className= "top">
                     <section className= "welcome">
                         <img className="background_index" src={Background} alt="movies-poster"/>
-                        <div className="text_on_image">Witajcie na <br/> filmowo-serialowych <br/> polecajkach!</div> 
+                        <div className="text_on_image">Welcome to <br/>movies and tv series <br/> 
+                                                        recommendations!</div> 
 
-                            <div className="text_welcome"><br />Cześć! Jesteśmy studentkami Informatyki, które w wolych chwilach uwielbiają oglądać filmy i seriale.<br /> Postanowiłyśmy podzielić
-                                się z Wami naszymi opiniami na temat tego, co właśnie obejrzałyśmy. <br />Zaglądajcie śmiało i koniecznie dajcie nam znać co sądzicie!</div>
+                            <div className="text_welcome"><br />Hi! We are computer science students who love watching movies and series 
+                                in their free time. <br /> We decided to share with you our opinions on what we just watched. 
+                                <br />Stay tuned and be sure to let us know what you think!</div>
                         
                         <div className="text_names"><h3>Justyna &amp; Magda</h3></div>
                     </section>
                 </section>
-
+                
+                <section className= "wish_list">
+                    <div className="wish_list" id="wish_list">
+                        <br />
+                        <p><strong>Wish list</strong></p>
+                        <p>Make a list of movies and series that you want us to review in the future</p>
+                        <button onClick={this.create_btn} type="button" id="create_btn">Create</button>
+                        <div className="w_list" id="w_list" ref={this.div_list}>
+                            <div className="border_l" id="border_l">
+                                <ul id="my_list">
+                                </ul>
+                            </div>
+                            <p><label>Your proposition:</label>
+                                <br />
+                                <input type="text" id="input_list" onKeyDown={this.pressed_enter} />
+                            </p>
+                        </div>
+                    </div>
+                </section>
+                <section className="nextto">
+                <div id="float">
+                    <div id="mail-div">
+                        <p>
+                        Maybe you want to recommend a movie or series to us? Write to us!
+                        <p><a href="mailto:254520@student.pwr.edu.pl">
+                            <img src="http://ppp5.pl/wp-content/uploads/2017/03/email-icon-23-300x300.png" width="30" height="30"
+                                alt="Email" id="index_transition"/> </a></p>
+                    </p>  
+                    </div>
+                    <div id="form_index">
+                        <form>
+                            <p>
+                                <label>
+                                    Rate us!
+                                    <Select options={options} />
+                                </label>
+                            </p>
+                            <p>
+                                <button onClick={this.showAlert} id="send_btn">Send</button>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </section>
+            <p>
+                <section class= "watch_on_average">
+                    <div class="avg_watch">
+                        <p><strong>Do you want to know how you compare to the average adult in the number of hours spent watching TV?</strong></p>
+                        <button onClick={this.check_avg} type="button" id="avg_btn">Check</button>
+                        <p id="result"></p>
+                    </div>
+                </section>
+            </p>
             </div>
         );
+    }
+    showAlert=()=> {
+        var alertThx = "Thank you for participating in the survey!";
+        alert (alertThx);
+     }
+     check_avg=()=> {
+        var hours = prompt("Enter the number of hours");
+        var num = parseFloat(hours);;
+
+        if(num > 4)
+        {
+            document.getElementById("result").innerHTML ="You spend more time watching TV than the average adult!";
+        }
+        else if(num === 4)
+        {
+            document.getElementById("result").innerHTML ="You spend exactly as much time watching TV as the average adult does";
+        }
+        else if(num < 4){
+            document.getElementById("result").innerHTML ="You spend less time watching TV than the average adult";
+        }
+        else{
+            document.getElementById("result").innerHTML ="You gave wrong values";
+        }
     }
 }
 export default Home
--- src/components/pages/TvSeries.js
+++ src/components/pages/TvSeries.js
@@ -1,12 +1,114 @@
 import React, {Component} from 'react';
 
 class TvSeries extends Component {
+    constructor(props) {
+        super(props);
+        this.img_mrrobot = React.createRef();
+        this.img_sherlock = React.createRef();
+      }
+    
     render() {
         return (
             <div className="container-fluid">
-                <h3>TvSeries</h3>
-            </div>
+                <section className="top">
+                    <div className="text_tvseries">
+                        <h1>Tv series Recommendations</h1>
+                    </div>
+                        <div className="dropdown drp">
+                        <div className="dropbtn">Navigate</div>
+                        <div className="dropdown-one">
+                            <div id="link1" className="dItem">Review
+                        <div className="dropdown-two">
+                            <a className="dItem" href="#sherlock">Sherlock</a>
+                            <a className="dItem" href="#mrrobot">Mr Robot</a>
+                        </div>
+                    </div>
+                    <a className="dItem" href="#text_ranking">Rank</a>
+                    <a className="dItem" href="#glosowanie">Vote</a>
+                    </div>
+                    </div>
+                <section className="diff">
+                    <div id="difusion">
+                        <img className = "bottom" src={require('../../assets/images/tvseries.jpg')} alt="poster"  id="img_sizes_tvseries"/>
+                        <img className = "top" src={require('../../assets/images/tv.jpg')} alt="movies poster" id="img_sizes_tvseries"/> 
+                    </div>
+                </section>
+            </section>
+
+            <section className="reviews">
+                <div className="text_tvseries">
+                    <h3 id="recenzje">Reviews</h3>
+                </div>
+                <img src="https://fwcdn.pl/fpo/89/92/528992/7864164.3.jpg" width="150" height="200" alt="sherlock"
+                        ref={this.img_sherlock} onMouseOver={this.zoomin_sherlock} onMouseOut={this.zoomout_sherlock}/>
+                <section className="Sherlock" id="media_query">
+                    <h3 id="sherlock">Sherlock</h3>
+                        <summary>Review</summary>
+                        <div>
+                        There is no person who has not heard of Sherlock Holmes. No wonder that various directors have decided to adapt the detective's adventures. One of them is the series "Sherlock". The action of the series takes place in the present. Military doctor, John Watson, after returning from a mission in Afghanistan, as a result of a coincidence meets detective Sherlock Holmes, with whom he soon lives on Baker Street. Soon they are together solving puzzles. A very important, if not the most important advantage of the series is the cast and acting. Benedict Cumberbatch fits perfectly into the role of Sherlock, taking the art of acting to the heights of absolute perfection. It should be added that the whole galaxy of second and third characters is also presented extremely skillfully, not only serving as a background for the actions of the two main protagonists. The well-composed music, neatly building the atmosphere and tension, also makes a great impression. After watching the first season, I knew that "Sherlock" would be high on my personal series rankings.
+                        The BBC production has everything that should be characteristic of any good cinema, i.e. interesting,
+                        perfectly written characters, a nuanced story and atmosphere,
+                        which captivates from the first minute. I sincerely recommend this series.
+                        </div>
+                    <aside>
+                        <a href="https://www.youtube.com/watch?v=Nj7ZSUkTTVI">See trailer</a><br />
+                        <a href="downloads/muzyka_z_serialu.mp3" download="sherlock holmes soundtrack">Download music from
+                            series</a>
+                    </aside>
+                </section>
+                <div className="img_mrrobot">
+                    <img src="https://www.galeriaplakatu.com/img/imagecache/87001-88000/602x904_product_media_87001-88000_robot_kopia_min-j.webp"
+                        width="150" height="200" alt="mrrobot" ref={this.img_mrrobot} onMouseOver={this.zoomin_mrrobot} onMouseOut={this.zoomout_mrrobot}/>
+                </div>
+                <section className="Mr_robot" id="media_query">
+                    <h3 id="mrrobot">Mr. Robot</h3>
+                        <summary>Review</summary>
+                        <div>
+                        <strong> Mr. Robot </strong> is an American production created by Sam Esmail with the help of Anonymous
+                        Content. The series tells the story of a destroyed world. A world where people are driven by selfish motives, dominated by large corporations that run the life of every average person.
+                        The main character, Eliot Alderson, is a young programmer suffering from social phobia. He works in a company dealing with online security, but after work, he devotes himself to his passion - hacking. Production is a logical puzzle, which on
+                        every step tries to deceive the viewer. It affects the human subconscious and mocks it at every possible opportunity. Dialogues and scenes are full of the obvious
+                        references to pop culture, cult productions such as: "Matrix",
+                        "Fight Club" or "Fight Club". The production's strength is the cast, and above all the main character - Rami Malek, who plays Eliot Alderson. An actor so charismatic, so perfectly reflecting the character of the character. On the one hand, hidden in itself,
+                        manifesting features of autism, and on the other damning
+                        reality and extremely lonely. I am sure that this already cult production will become the subject of many
+                        discussion and interpretation among pop culture fans. I personally recommend it. And you watched this series? Do you have it in your plans? Write to us
+                        </div>
+                    <div className="sherlock_aside">
+                        <aside>
+                            <br/>
+                            <a href="https://www.youtube.com/watch?v=xIBiJ_SzJTA">See trailer</a><br />
+                            <a href="https://fwcdn.pl/fpo/37/95/733795/7689969.3.jpg" download="Mr.Robot">Download music from series</a>
+                        </aside>
+                    </div>
+                </section>
+        </section>
+        </div>
         );
+    }
+
+    zoomin_sherlock =()=>{
+        const imgSherlock = this.img_sherlock.current;
+        imgSherlock.style.height = '230px';
+        imgSherlock.style.width = '200px';
+    }
+
+    zoomout_sherlock=()=>{
+        const imgSherlock = this.img_sherlock.current;
+        imgSherlock.style.height = '210px';
+        imgSherlock.style.width = '150px';
+    }
+
+    zoomin_mrrobot=()=>{
+        const imgMrrobot = this.img_mrrobot.current;
+        imgMrrobot.style.height = '230px';
+        imgMrrobot.style.width = '200px';
+    }
+
+    zoomout_mrrobot=()=>{
+        const imgMrrobot = this.img_mrrobot.current;
+        imgMrrobot.style.height = '210px';
+        imgMrrobot.style.width = '150px';
     }
 }
 export default TvSeries
